dc3e5eadd3a5ddc5e7dca0d11be307989b1509f6
Michael Campitiello <michaelrcampi@gmail.com>
2022-05-24 11:08:05 -0700

    Reformatting HTML and Renderer
    Rewrote tmpl/html files to .gohtml and got extension to make them look pretty. 
    Created layout file and reformatted page files to accept the layout adding only custom html. 
    
    created a new render function to parse through html files to properly render.

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -4,14 +4,59 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"path/filepath"
 )
+
+var functions = template.FuncMap{}
 
 //RenderTemplate renders templates using html/template
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
+
+	_, err := RenderTemplateTest(w)
+	if err != nil {
+		fmt.Println("Erorr getting template cache:", err)
+	}
+
 	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
-	err := parsedTemplate.Execute(w, nil)
+	err = parsedTemplate.Execute(w, nil)
 	if err != nil {
 		fmt.Println("error parsing template:", err)
 		return
 	}
+}
+
+func RenderTemplateTest(w http.ResponseWriter) (map[string]*template.Template, error) {
+
+	myCache := map[string]*template.Template{}
+
+	//find anything that ends in .page.gohtml (* is wildcard)
+	pages, err := filepath.Glob("./template/*.page.gohtml")
+	if err != nil {
+		return myCache, err
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+		fmt.Println("Page is currently", page)
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
+		if err != nil {
+			return myCache, err
+		}
+
+		matches, err := filepath.Glob("./template/*.layout.gohtml")
+		if err != nil {
+			return myCache, err
+		}
+
+		if len(matches) > 0 {
+			ts, err = ts.ParseGlob("./templates/*.layout.gohtml")
+			if err != nil {
+				return myCache, err
+			}
+		}
+
+		myCache[name] = ts
+	}
+
+	return myCache, nil
 }
