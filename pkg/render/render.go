f6f508c81ee2de7c2d042ade8623d3b43fa7e300
Michael R Campitiello <michaelrcampi@gmail.com>
2022-06-13 15:25:59 -0700

    Added Comments

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -7,6 +7,9 @@ import (
 	"path/filepath"
 )
 
+//A map of fuctions that can be used
+//not built into the template 'language'
+//but Go allows us to pass them in and use them this way.
 var functions = template.FuncMap{}
 
 //RenderTemplate renders templates using html/template
@@ -18,6 +21,7 @@ func RenderTemplate(w http.ResponseWriter, tmpl string) {
 	}
 
 	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+
 	err = parsedTemplate.Execute(w, nil)
 	if err != nil {
 		fmt.Println("error parsing template:", err)
@@ -36,18 +40,25 @@ func RenderTemplateTest(w http.ResponseWriter) (map[string]*template.Template, e
 	}
 
 	for _, page := range pages {
+		//extracts name of file
 		name := filepath.Base(page)
+		//print to console name of file grabbed
 		fmt.Println("Page is currently", page)
+
+		//creating a template set [ts]
 		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
 		if err != nil {
 			return myCache, err
 		}
 
+		//check to see if there are any layouts that match this template
 		matches, err := filepath.Glob("./template/*.layout.gohtml")
 		if err != nil {
 			return myCache, err
 		}
 
+		//checks to see if # of matches is greater than 0
+		//if TRUE parse the layout and match it to the template.
 		if len(matches) > 0 {
 			ts, err = ts.ParseGlob("./templates/*.layout.gohtml")
 			if err != nil {
@@ -55,6 +66,7 @@ func RenderTemplateTest(w http.ResponseWriter) (map[string]*template.Template, e
 			}
 		}
 
+		//add template set to myCache map
 		myCache[name] = ts
 	}
 
