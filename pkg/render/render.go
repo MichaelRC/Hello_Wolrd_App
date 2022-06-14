a14240daeb20b0f1d2f20b1c511932b9d6716571
Michael R Campitiello <michaelrcampi@gmail.com>
2022-06-14 12:05:04 -0700

    Redid RenderTemplate

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -1,8 +1,10 @@
 package render
 
 import (
+	"bytes"
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
 	"path/filepath"
 )
@@ -15,45 +17,57 @@ var functions = template.FuncMap{}
 //RenderTemplate renders templates using html/template
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
 
-	_, err := RenderTemplateTest(w)
+	tc, err := CreateTemplateCache()
 	if err != nil {
-		fmt.Println("Erorr getting template cache:", err)
+		//closes the program and logs the error
+		log.Println("Error 1", err)
+		log.Fatal(err)
 	}
 
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
-
-	err = parsedTemplate.Execute(w, nil)
-	if err != nil {
-		fmt.Println("error parsing template:", err)
-		return
+	t, ok := tc[tmpl]
+	if !ok {
+		log.Println("Error 2", err)
+		log.Fatal(err)
 	}
+
+	buf := new(bytes.Buffer)
+
+	_ = t.Execute(buf, nil)
+
+	_, err = buf.WriteTo(w)
+	if err != nil {
+		fmt.Println("Error writing template to browser", err)
+	}
+
 }
 
-func RenderTemplateTest(w http.ResponseWriter) (map[string]*template.Template, error) {
+// CreareTemplateCache creates a template cache as a map
+func CreateTemplateCache() (map[string]*template.Template, error) {
 
 	myCache := map[string]*template.Template{}
 
 	//find anything that ends in .page.gohtml (* is wildcard)
 	pages, err := filepath.Glob("./template/*.page.gohtml")
 	if err != nil {
+		log.Println("Error CTC 1", err)
 		return myCache, err
 	}
 
 	for _, page := range pages {
 		//extracts name of file
 		name := filepath.Base(page)
-		//print to console name of file grabbed
-		fmt.Println("Page is currently", page)
 
 		//creating a template set [ts]
 		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
 		if err != nil {
+			log.Println("Error CTC 2", err)
 			return myCache, err
 		}
 
 		//check to see if there are any layouts that match this template
 		matches, err := filepath.Glob("./template/*.layout.gohtml")
 		if err != nil {
+			log.Println("Error CTC 3", err)
 			return myCache, err
 		}
 
@@ -62,6 +76,7 @@ func RenderTemplateTest(w http.ResponseWriter) (map[string]*template.Template, e
 		if len(matches) > 0 {
 			ts, err = ts.ParseGlob("./templates/*.layout.gohtml")
 			if err != nil {
+				log.Println("Error CTC 4", err)
 				return myCache, err
 			}
 		}
